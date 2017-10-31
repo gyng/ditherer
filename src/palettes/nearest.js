a46fa1712ae9eb7fa095c03dd5348c86ce11e7f3
Ng Guoyou <fauxface+github@gmail.com>
2017-11-01 06:18:20 +0800

    Revert nearest palette to simple bucketing
    Too slow for large levels

--- src/palettes/nearest.js
+++ src/palettes/nearest.js
@@ -1,74 +1,29 @@
 // @flow
 
-import { RANGE, COLOR_DISTANCE_ALGORITHM } from "constants/controlTypes";
-import { RGB_NEAREST } from "constants/color";
-import { colorDistance } from "utils";
+import { RANGE } from "constants/controlTypes";
 
-import type { ColorRGBA, ColorDistanceAlgorithm } from "types";
+import type { ColorRGBA } from "types";
 
 const optionTypes = {
-  levels: { type: RANGE, range: [1, 256], default: 2 },
-  colorDistanceAlgorithm: COLOR_DISTANCE_ALGORITHM
+  levels: { type: RANGE, range: [1, 256], default: 2 }
 };
 
 const defaults = {
-  levels: optionTypes.levels.default,
-  colorDistanceAlgorithm: RGB_NEAREST
-};
-
-const palettesMemo = {};
-
-const makePalette = (levels: number) => {
-  palettesMemo[levels] = [];
-  const step = 255 / (levels - 1);
-  for (let i = 0; i < levels; i += 1) {
-    for (let j = 0; j < levels; j += 1) {
-      for (let k = 0; k < levels; k += 1) {
-        for (let l = 0; l < levels; l += 1) {
-          palettesMemo[levels].push([
-            Math.floor(i * step),
-            Math.floor(j * step),
-            Math.floor(k * step),
-            Math.floor(l * step)
-          ]);
-        }
-      }
-    }
-  }
+  levels: optionTypes.levels.default
 };
 
 // Gets nearest color
 const getColor = (
   color: ColorRGBA,
-  options: {
-    levels: number,
-    colorDistanceAlgorithm: ColorDistanceAlgorithm
-  } = defaults
+  options: { levels: number } = defaults
 ): ColorRGBA => {
-  const { levels, colorDistanceAlgorithm } = options;
+  const step = 255 / (options.levels - 1);
 
-  if (typeof palettesMemo[levels] === "undefined") {
-    makePalette(levels);
-  }
-  const palette = palettesMemo[levels];
-
-  let min = null;
-  let minDistance = 0;
-
-  for (let i = 0; i < palette.length; i += 1) {
-    const pc = palette[i];
-    const distance = colorDistance(pc, color, colorDistanceAlgorithm);
-
-    if (min === null) {
-      min = pc;
-      minDistance = distance;
-    } else if (distance < minDistance) {
-      min = pc;
-      minDistance = distance;
-    }
-  }
-
-  return !min ? color : min;
+  // $FlowFixMe
+  return color.map(c => {
+    const bucket = Math.round(c / step);
+    return Math.round(bucket * step);
+  });
 };
 
 export default {
