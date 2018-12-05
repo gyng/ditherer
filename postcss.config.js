c33dd606c9cfbc7499617e36c8bfba19074b8ece
Ng Guoyou <fauxface+github@gmail.com>
2018-12-06 01:06:32 +0800

    Add postcss-mixins

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   plugins: {
+    "postcss-mixins": {
+      mixinsDir: "src/styles/postcss/mixins"
+    },
     "postcss-preset-env": {
       stage: 0,
-      importFrom: ["src/styles/postcss/customMedia.css"]
+      importFrom: ["src/styles/postcss/customMedia.scss"]
     },
     cssnano: {},
     "postcss-reporter": {}
