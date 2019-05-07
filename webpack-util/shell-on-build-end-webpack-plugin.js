b72df4b303494b4194b63944111bc5e854dd3877
Ng Guoyou <gyng@users.noreply.github.com>
2019-05-07 14:12:09 +0800

    Add option to kill child for ShellOnBuildEndWebpack

--- webpack-util/shell-on-build-end-webpack-plugin.js
+++ webpack-util/shell-on-build-end-webpack-plugin.js
@@ -1,26 +1,60 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 /**
  * @typedef {Object} Options
  * @property {string} command - The command to run
  * @property {boolean=} once - Whether to run the script only once
+ * @property {boolean=} killExisting - Whether to kill existing running
+ *                                     commands before running again
+ * @property {boolean=} debug
+ */
+
+/**
  * @param options {Options}
+ * @property {boolean} ran
+ * @property {import("child_process").ChildProcess} child
  */
 module.exports = function ShellOnBuildEndPlugin(options) {
   this.ran = false;
+  this.child = null;
+
+  const debug = message => {
+    if (options.debug) {
+      process.stdout.write(`ℹ ShellOnBuildPlugin: ${message}\n`);
+    }
+  };
+
+  const killUnwantedExistingChild = () => {
+    if (options.killExisting && this.child && !this.child.killed) {
+      this.child.kill("SIGTERM");
+      debug(`killed ${this.child.pid} ${this.child.killed}\n`);
+    }
+  };
 
   this.apply = compiler => {
     compiler.hooks.afterEmit.tap("AfterEmitPlugin", () => {
       if (!this.ran) {
-        exec(options.command, (err, stdout, stderr) => {
-          if (stdout) process.stdout.write(stdout);
-          if (stderr) process.stderr.write(stderr);
+        killUnwantedExistingChild();
+
+        this.child = spawn(options.command, {
+          stdio: "inherit",
+          shell: true
         });
+
+        this.child.on("close", () => {
+          killUnwantedExistingChild();
+        });
+
+        debug(`[${this.child.pid}] ${options.command}`);
 
         if (options.once) {
           this.ran = true;
         }
       }
+    });
+
+    compiler.hooks.beforeRun.tap("BeforeRunPlugin", () => {
+      killUnwantedExistingChild();
     });
   };
 };
