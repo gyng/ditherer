56a076ce0030fd8ea88bda1f86df0736462b9d55
Guoyou <gyng@users.noreply.github.com>
2019-12-05 15:07:36 +0800

    Temporarily add ?? and ?. until they are in @babel/typescript

--- .babelrc.js
+++ .babelrc.js
@@ -5,6 +5,10 @@ const plugins = [
   "@babel/plugin-syntax-dynamic-import",
   "@babel/plugin-proposal-class-properties",
   "@babel/proposal-object-rest-spread",
+  // These 2 will be included in babel/typescript presets soon
+  // Ref: https://github.com/babel/babel/issues/10690
+  "@babel/plugin-proposal-nullish-coalescing-operator",
+  "@babel/plugin-proposal-optional-chaining",
   ...(DEV ? ["react-hot-loader/babel"] : [])
 ];
 
